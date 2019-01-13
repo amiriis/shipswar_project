e9b5ebf56ee35824f7b13d75146c77c643907263
Amirhossein Mahmoodi <amir.mahmoodi74@gmail.com>
2019-01-13 23:39:32 +0330

    config.php -- configure url site and database and hash

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Address App Config
+ */
+define("URL", "http://localhost/shipswar_project");
+define("SITE_URL", "/shipswar_project/");
+define("SITE_NAME","جنگ کشتی ها");
+define("URL_CSS", "public/css/");
+define("URL_JS", "public/js/");
+define("URL_IMAGE", "public/images/");
+
+/**
+ * Database Config
+ */
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'shipswar_database');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_PREFIX', 'sw_d_');
+
+/**
+ * HASH
+ */
+define('KEY_PASSWORD', 'shipsWar');
+define('ALGO_PASSWORD', 'MD5');
