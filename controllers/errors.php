cd6559861fdf3ef5dbc9c65045d311eeee1e8ed0
Amirhossein Mahmoodi <amir.mahmoodi74@gmail.com>
2019-01-13 23:43:22 +0330

    controllers/errors.php -- errors handling

---
+++ controllers/errors.php
@@ -0,0 +1,11 @@
+<?php
+
+class errors{
+  function Not_Exist_Controller(){
+    echo "this controller is not exist!";
+  }
+
+  function Not_Exist_Function(){
+    echo "this function is not exist!";
+  }
+}
